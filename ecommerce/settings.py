933213c8f950c75db4ce4f536086e11d9f85cc20
Red Bill <redbillb@gmail.com>
2018-09-08 23:55:01 -0300

    Ajuste no arquivo settings para usar o sqlite tambem em producao

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
