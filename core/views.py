1da41f60770cf0e13e4a2f955c5c62a7f3bc4996
Red Bill <redbillb@gmail.com>
2018-09-10 16:49:29 -0300

    Ajustando viasualização de cada produto quando clicado no botao ou na foto

--- core/views.py
+++ core/views.py
@@ -3,13 +3,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 def contact(request):
-	return render(request, 'contact.html')
-
-def product(request):
-	return render(request, 'product.html')
+    return render(request, 'contact.html')
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,4 @@
-"""ecommerce URL Configuration
-
+"""djangoecommerce URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
@@ -20,9 +19,8 @@ from core import views
 from catalog import views as views_catalog
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^contato/$', views.contact, name='contact'),
-	url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/',include('catalog.urls', namespace='catalog')),
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
